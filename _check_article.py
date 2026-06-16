59c7bb8cbdb09d2903fbc6170b2c4847adcf6d1f
calvinzqz <myzqz@163.com>
2026-06-17 01:17:41 +0800

    feat: 新增勒索病毒技术深度调研报告（2026-06-16）
    - 15大勒索家族、64篇权威文献、9个SVG技术图谱
    - 分类：安全，位于洞察列表首位
    - 清理历史临时脚本

--- _check_article.py
+++
@@ -1,8 +0,0 @@
-import re
-
-html = open('content/articles/2026-05-aicms-intro/index.html', 'rb').read()
-
-# Check meta tags
-for line in html.split(b'\n'):
-    if b'name="' in line and (b'category' in line or b'tags' in line or b'date' in line):
-        print(repr(line[:200]))
